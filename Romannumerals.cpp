55fd8db9c0d838ee542a37d4003c99f6209512a2
thedeepestreality <zetoster@gmail.com>
2021-10-29 00:54:09 +0300

    roman nums var1

--- Romannumerals.cpp
+++ Romannumerals.cpp
@@ -14,58 +14,47 @@ int main() {
 		for (int count = 0; count < a2; ++count)
 			std::cout << "C";
 	}
-	else if (a2 == 4) {
-		std::cout << "CD";
-	}
-	else if (a2 == 5) {
-		std::cout << "D";
-	}
-	else if (a2 > 5 and a2 < 9) {
-		std::cout << "D";
-		for (int count = 5; count < a2; ++count) {
-			std::cout << "C";
+	else { 
+		if (a2 < 9){
+			if (a2 == 4)
+				std::cout << "C";
+			std::cout << "D";
+			for (int count = 5; count < a2; ++count)
+		 		std::cout << "C";
 		}
-	}
-	else if (a2 == 9) {
-		std::cout << "CM";
+		else
+			std::cout << "CM";
 	}
 	if (a3 < 4) {
 		for (int count = 0; count < a3; ++count)
 			std::cout << "X";
 	}
-	else if (a3 == 4) {
-		std::cout << "XL";
-	}
-	else if (a3 == 5) {
-		std::cout << "L";
-	}
-	else if (a3 > 5 and a3 < 9) {
-		std::cout << "L";
-		for (int count = 5; count < a3; ++count) {
-			std::cout << "X";
+	else { 
+		if (a3 < 9){
+			if (a3 == 4)
+				std::cout << "X";
+			std::cout << "L";
+			for (int count = 5; count < a3; ++count)
+		 		std::cout << "X";
 		}
-	}
-	else if (a3 == 9) {
-		std::cout << "XC";
+		else
+			std::cout << "XC";
 	}
 	if (a4 < 4) {
 		for (int count = 0; count < a4; ++count)
 			std::cout << "I";
 	}
-	else if (a4 == 4) {
-		std::cout << "IV";
-	}
-	else if (a4 == 5) {
-		std::cout << "V";
-	}
-	else if (a4 > 5 and a4 < 9) {
-		std::cout << "V";
-		for (int count = 5; count < a4; ++count) {
-			std::cout << "I";
+	else { 
+		if (a4 < 9){
+			if (a4 == 4)
+				std::cout << "I";
+			std::cout << "V";
+			for (int count = 5; count < a4; ++count)
+		 		std::cout << "I";
 		}
+		else
+			std::cout << "IL";
 	}
-	else if (a4 == 9) {
-		std::cout << "IX";
-	}
+	std::cout << "\n";
 	return 0;
 }
