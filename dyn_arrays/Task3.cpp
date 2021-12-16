2a03bb6bb56ac0d3c438db7a779247fa1d655fef
HerePeaky <92159037+HerePeaky@users.noreply.github.com>
2021-12-17 02:23:08 +0300

    Update Task3.cpp
    Add an argument of filling direction

--- dyn_arrays/Task3.cpp
+++ dyn_arrays/Task3.cpp
@@ -1,38 +1,43 @@
 #include<iostream>
-void Task3SnakeArray(int** x, int n)
-{
-	for (int i = 0; i < n; ++i)
-	{
-		x[i] = new int[n];
-	}
-	for (int j = 0; j < n; j+=2)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			x[i][j] = n * j + i;
-		}
-	}
-	for (int j = 1; j < n; j += 2)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			x[i][j] = n * j +n - i-1;
-		}
-	}
-	for (int j = 0; j < n; ++j)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			std::cout << x[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+
+void Task3SnakeArray(int** M, int n, bool d) {
+    int k = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == d) {
+            for (int j = 0; j < n; ++j) {
+                ++k;
+                M[i][j] = k;
+           }
+        }
+        else {
+            for (int j = n - 1; j >= 0; --j) {
+                ++k;
+                M[i][j] = k;
+            }
+        }
+    }
 }
-int main()
-{
-	int n;
-	std::cout << "Enter n: = ";
-	std::cin >> n;
-	int** answer = new int*[n];
-	Task3SnakeArray(answer, n);
+
+int main() {
+    int n;
+    bool d;
+    std::cout << "Enter n: = ";
+    std::cin >> n;
+    std::cout << "Enter direction of filling ( 0 is >>>, 1 is <<<): ";
+    std::cin >> d;
+    int** x = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        x[i] = new int[n];
+    }
+    Task3SnakeArray(x, n, d);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << x[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    for (int i = 0; i < n; ++i) {
+        delete[] x[i];
+    }
+    delete[] x;
 }
