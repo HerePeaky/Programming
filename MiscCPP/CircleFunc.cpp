608af402ba09a6732729af18a92bb9f0ee8713f6
HerePeaky <92159037+HerePeaky@users.noreply.github.com>
2021-12-18 19:56:49 +0300

    Update CircleFunc.cpp

--- MiscCPP/CircleFunc.cpp
+++ MiscCPP/CircleFunc.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-int circlefunc(int R) {
+int circlefunc(unsigned long int R) {
     int counter = 0;
-    for (int x = 0; x <= R; ++x) {
-        for (int y = 0; y <= R; ++y)
+    for (unsigned long int x = 0; x <= R; ++x) {
+        for (unsigned long int y = 0; y <= R; ++y)
         {
-            if ((x * x + y * y) <= R*R) {
+            if ((x * x + y * y)     <= R * R) {
                 ++counter;
             }
         }
